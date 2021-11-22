aea2ab9871ef3556e54ae5498b32d748e08617a9
Jigsaw-23122002 <harshnag23@gmail.com>
2021-11-22 19:01:27 +0530

    Haga

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
-import Login from './Login';
+import Login from './login';
 import CreateRoom from './CreateRoom';
 import JoinRoom from './JoinRoom';
 import './index.css'
 function App() {
-  // const [roomname, setRoomName] = useState("");
-  // const [showChat, setShowChat] = useState(false);
-  // const [room_code, setRoom_Code] = useState("");
-
-  // const joinRoom = () => {
-  //   if (roomname !== "" && room_code !== "") {
-  //     socket.emit("join_room", roomname);
-  //     setShowChat(true);
-  //   }
-  // };
   return (
     <>
       <div> < Login />  </div>
