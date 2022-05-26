9f1c97c6b374b42343799d0de541a2e75df45a07
Neel-Shah-29 <84740927+Neel-Shah-29@users.noreply.github.com>
2022-05-26 13:35:16 +0530

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div class="footer">
-            <p className="text-center">Copyright &copy; Sociablast.com</p>
+            <p className="text-center">Copyright &copy; Chatblast.com</p>
         </div>
     )
 }
